82f38d5447aa90855328504914a2b4e555d7f769
TaeeunKim <xodms0309@naver.com>
2022-05-17 23:01:00 +0900

    [구현 & 코너케이스] 5월 17일 - Update

--- 5월 10일 - 구현 & 코너케이스/16236.cpp
+++ 5월 10일 - 구현 & 코너케이스/16236.cpp
@@ -4,13 +4,14 @@
 #include <tuple>
 #include <algorithm>
 using namespace std;
-int dx[4] = {0, 0, -1, 1};
-int dy[4] = {-1, 1, 0, 0 };
-queue<pair<int, int>>q;
-vector <tuple<int, int, int>>fish;
+int dx[4] = { 0, 0, -1, 1 };
+int dy[4] = { -1, 1, 0, 0 };
 int shark = 2;
+tuple<int, int, int> bfs(vector<vector<int>>& v, int n, int x, int y) {
+	vector<vector<int>>visited(n, vector<int>(n, -1));
+	vector <tuple<int, int, int>>fish;
+	queue<pair<int, int>>q;
 
-pair<int,int> bfs(vector<vector<int>> &v, vector<vector<int>> &visited, int n, int x, int y) {
 	visited[x][y] = 1;
 	q.push({ x,y });
 
@@ -20,31 +21,35 @@ pair<int,int> bfs(vector<vector<int>> &v, vector<vector<int>> &visited, int n, i
 		q.pop();
 
 		for (int i = 0; i < 4; i++) {
-			int next_x = x + dx[i];
-			int next_y = y + dy[i];
+			int next_x = current_x + dx[i];
+			int next_y = current_y + dy[i];
 
 			if (next_x < 0 || next_x >= n || next_y < 0 || next_y >= n) continue;
 			if (visited[next_x][next_y] != -1 || v[next_x][next_y] > shark) continue;
 
+
+
 			//물고기가 있는 경우
 			if (v[next_x][next_y] > 0 && v[next_x][next_y] < shark) {
-				fish.push_back({ visited[next_x][next_y], next_x, next_y });
+				fish.push_back({ visited[current_x][current_y], next_x, next_y });
 			}
 
-			visited[next_x][next_y] = visited[x][y] + 1;
-			q.push({next_x, next_y});
-			
+			visited[next_x][next_y] = visited[current_x][current_y] + 1;
+			q.push({ next_x, next_y });
+
 		}
 	}
-	
+
 	sort(fish.begin(), fish.end());
-	if (fish.size() == 0) return { -1, -1 };
+
+	//더이상 물고기를 먹을 수 없음
+	if (fish.size() == 0) return { -1, -1, -1 };
 
 	int min_dist = get<0>(fish[0]);
 	int min_x = get<1>(fish[0]);
 	int min_y = get<2>(fish[0]);
 
-	return { min_x, min_y };
+	return { min_dist, min_x, min_y };
 
 }
 int main() {
@@ -62,17 +67,23 @@ int main() {
 			}
 		}
 	}
+
 	int t = 0;
 	int eat = 0;
+
 	while (true) {
-		vector<vector<int>>visited(n, vector<int>(n, -1));
-		pair<int, int> p = bfs(v, visited, n, start_x, start_y);
-		cout << p.first << p.second;
-		if (p.first == -1 && p.second == -1) break;
-		t += visited[p.first][p.second];
-		v[p.first][p.second] = 0;
-		start_x = p.first;
-		start_y = p.second;
+		tuple<int, int, int> ti = bfs(v, n, start_x, start_y);
+
+		int dist = get<0>(ti);
+		int x = get<1>(ti);
+		int y = get<2>(ti);
+
+		if (dist == -1 && x == -1 && y == -1) break;
+
+		t += dist;
+		v[x][y] = 0;
+		start_x = x;
+		start_y = y;
 		eat++;
 		if (eat == shark) {
 			shark++;
